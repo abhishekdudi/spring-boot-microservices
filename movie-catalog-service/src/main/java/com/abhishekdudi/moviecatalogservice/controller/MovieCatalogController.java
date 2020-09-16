1e2fcdccd6d35539d916a5eb3c39bd9702dab811
Abhishek Dudi <abhishekdudi@icloud.com>
2020-09-16 18:16:28 -0500

    consumed movie-info api in movie-catalog api

--- movie-catalog-service/src/main/java/com/abhishekdudi/moviecatalogservice/controller/MovieCatalogController.java
+++ movie-catalog-service/src/main/java/com/abhishekdudi/moviecatalogservice/controller/MovieCatalogController.java
@@ -1,11 +1,15 @@
 package com.abhishekdudi.moviecatalogservice.controller;
 
 import com.abhishekdudi.moviecatalogservice.model.CatalogItem;
+import com.abhishekdudi.moviecatalogservice.model.Movie;
+import com.abhishekdudi.moviecatalogservice.model.RatingDetails;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
-import java.util.Collections;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @RestController
@@ -14,8 +18,28 @@ public class MovieCatalogController {
 
     @RequestMapping("/{id}")
     public List<CatalogItem> getCatalogs(@PathVariable(name = "id") String id) {
-        return Collections.singletonList(
-                new CatalogItem("Avengers", "Motion Picture", 10)
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        // get all the rated movies by the user
+        List<RatingDetails> ratings = Arrays.asList(
+                new RatingDetails("1234", 10),
+                new RatingDetails("5678", 7)
         );
+
+        // for each movie, call movie info service and get details
+        List<CatalogItem> catalogList = new ArrayList<>();
+
+        for (RatingDetails ratingDetails : ratings) {
+            Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + ratingDetails.getMovieId(), Movie.class);
+            catalogList.add(new CatalogItem(movie.getName(), "Motion Picture", ratingDetails.getRating()));
+        }
+
+        // put them together
+
+//        return Collections.singletonList(
+//                new CatalogItem("Avengers", "Motion Picture", 10)
+//        );
+        return catalogList;
     }
 }
---
+++ movie-catalog-service/src/main/java/com/abhishekdudi/moviecatalogservice/model/RatingDetails.java
@@ -0,0 +1,28 @@
+package com.abhishekdudi.moviecatalogservice.model;
+
+public class RatingDetails {
+
+    private String movieId;
+    private int rating;
+
+    public RatingDetails(String movieId, int rating) {
+        this.movieId = movieId;
+        this.rating = rating;
+    }
+
+    public String getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(String movieId) {
+        this.movieId = movieId;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+}
