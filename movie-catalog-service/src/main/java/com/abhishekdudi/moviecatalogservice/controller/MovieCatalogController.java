2da4b991e2bde4605401b7c2c1f76d11549fa3bf
Abhishek Dudi <abhishekdudi@icloud.com>
2020-09-16 22:38:58 -0500

    used streams

--- movie-catalog-service/src/main/java/com/abhishekdudi/moviecatalogservice/controller/MovieCatalogController.java
+++ movie-catalog-service/src/main/java/com/abhishekdudi/moviecatalogservice/controller/MovieCatalogController.java
@@ -1,14 +1,17 @@
 package com.abhishekdudi.moviecatalogservice.controller;
 
-import com.abhishekdudi.moviecatalogservice.model.*;
+import com.abhishekdudi.moviecatalogservice.model.CatalogItem;
+import com.abhishekdudi.moviecatalogservice.model.CatalogList;
+import com.abhishekdudi.moviecatalogservice.model.Movie;
+import com.abhishekdudi.moviecatalogservice.model.UserRatingsList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/catalog")
@@ -23,15 +26,25 @@ public class MovieCatalogController {
         // get all the rated movies by the user
         UserRatingsList ratings = restTemplate.getForObject("http://localhost:8083/ratings/users/"+ userId, UserRatingsList.class);
 
-        List<CatalogItem> itemList = new ArrayList<>();
+        // without using streams
+//        List<CatalogItem> itemList = new ArrayList<>();
+//
+//        for (RatingDetails ratingDetails : ratings.getUserRatings()) {
+//            // for each movie, call movie info service and get details
+//            Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + ratingDetails.getMovieId(), Movie.class);
+//
+//            // put them together
+//            itemList.add(new CatalogItem(movie.getName(), "Motion Picture", ratingDetails.getRating()));
+//        }
 
-        for (RatingDetails ratingDetails : ratings.getUserRatings()) {
+        // using streams
+        List<CatalogItem> itemList = ratings.getUserRatings().stream().map(ratingDetails -> {
             // for each movie, call movie info service and get details
             Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + ratingDetails.getMovieId(), Movie.class);
 
             // put them together
-            itemList.add(new CatalogItem(movie.getName(), "Motion Picture", ratingDetails.getRating()));
-        }
+            return new CatalogItem(movie.getName(), "Motion Picture", ratingDetails.getRating());
+        }).collect(Collectors.toList());
 
         CatalogList catalogList = new CatalogList();
         catalogList.setCatalogItemList(itemList);
